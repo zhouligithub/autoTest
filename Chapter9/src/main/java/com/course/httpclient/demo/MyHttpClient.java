055ac5229d34cbcd0d89e6d8d09d72637e4511bb
zhouligithub <544988161@qq.com>
2019-03-28 18:48:14 +0800

    testNGDome2

---
+++ Chapter9/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,12 @@
+package com.course.httpclient.demo;
+
+import org.apache.http.client.methods.HttpGet;
+
+public class MyHttpClient {
+    @Test
+    public void test1(){
+        //用来存放我们的结果
+        String result;
+        HttpGet get=new
+    }
+}
